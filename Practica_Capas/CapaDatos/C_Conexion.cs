7be44026a6476e9e638427ed107210c95053e308
Fcabanas7 <83811519+Fcabanas7@users.noreply.github.com>
2021-05-07 03:38:09 -0500

    Add files via upload

---
+++ Practica_Capas/CapaDatos/C_Conexion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class C_Conexion
+    {
+        //Modificar DB y tabla
+        private SqlConnection Conexion = new SqlConnection("Server=DESKTOP-443JN4R\\SQLEXPRESS;DataBase= Practica;Integrated Security=true");
+
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+                Conexion.Open();
+            return Conexion;
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+                Conexion.Close();
+            return Conexion;
+        }
+    }
+}
